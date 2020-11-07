b98281aa2dced046919d2dbdb30491b367f57dc1
eshion <eshion@qq.com>
2020-11-07 18:17:35 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -142,10 +142,10 @@ cat <<-EOF > /caddybin/Caddyfile
   reverse_proxy @v2 127.0.0.1:2333
 
   @door {
-    header url *.eshion.*
+    header_regexp url url ([a-f0-9]+).eshion.*
   }
   reverse_proxy @door 127.0.0.1:3333 {
-    header_up Host {>url}
+    header_up Host {http.regexp.url.1}
     header_up X-Real-IP {remote}
     header_up X-Forwarded-For {remote}
     header_up X-Forwarded-Port {port}
